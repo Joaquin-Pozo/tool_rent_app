2d0bf82fc8f0752e920ca96b5288ef0e707521ee
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-08-23 18:01:37 -0400

    Configuración de proyecto en Spring Boot

---
+++ src/main/java/com/myproject/tool_rent_app/ToolRentAppApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.tool_rent_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolRentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolRentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/tool_rent_app/ToolRentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.tool_rent_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolRentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
