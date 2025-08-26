35620b27bd172491a899bf7c1160d54e9fabeaba
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-08-26 17:51:06 -0400

    New ToolEntity class created.

---
+++ src/main/java/com/myproject/tool_rent_app/entities/ToolEntity.java
@@ -0,0 +1,38 @@
+package com.myproject.tool_rent_app.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tools")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToolEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private BigDecimal replacementCost;
+
+    // Available, Loaned, Under repair, Unserviceable
+    private String currentState;
+
+    private BigDecimal price;
+
+    private int stock;
+
+}
