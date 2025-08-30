267c2e21fbe1db725944e27ff60c467fd1ed61d2
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-08-30 12:34:57 -0400

    Updated persistent layer with new KardexEntity class for recording all movements affecting the inventory.

---
+++ src/main/java/com/myproject/tool_rent_app/entities/KardexEntity.java
@@ -0,0 +1,43 @@
+package com.myproject.tool_rent_app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "kardex")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KardexEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "tool_id")
+    private ToolEntity tool;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private ClientEntity client;
+
+    @ManyToOne
+    @JoinColumn(name = "loan_id")
+    private LoanEntity loan;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id",  nullable = false)
+    private KardexTypeEntity type;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private Date movementDate;
+
+}
