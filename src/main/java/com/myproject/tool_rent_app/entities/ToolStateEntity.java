d395fc6784fd74acfaf831e432aa00b8b5050180
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-08-29 21:38:21 -0400

    Updated persistent layer with new ToolStateEntity class for tool states: Available, Loaned, Under repair, Unserviceable.

---
+++ src/main/java/com/myproject/tool_rent_app/entities/ToolStateEntity.java
@@ -0,0 +1,21 @@
+package com.myproject.tool_rent_app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tool_states")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToolStateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name; // Available, Loaned, Under repair, Unserviceable
+}
