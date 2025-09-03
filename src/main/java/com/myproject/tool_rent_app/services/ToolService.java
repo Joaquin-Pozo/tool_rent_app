6afaaf425301dd84849e0535544bcf074e6ac2f8
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-09-02 20:51:51 -0400

    Updated createTool method and added deleteTool method to delete unserviceable tools.

--- src/main/java/com/myproject/tool_rent_app/services/ToolService.java
+++ src/main/java/com/myproject/tool_rent_app/services/ToolService.java
@@ -3,9 +3,11 @@ package com.myproject.tool_rent_app.services;
 import com.myproject.tool_rent_app.entities.KardexEntity;
 import com.myproject.tool_rent_app.entities.KardexTypeEntity;
 import com.myproject.tool_rent_app.entities.ToolEntity;
+import com.myproject.tool_rent_app.entities.ToolStateEntity;
 import com.myproject.tool_rent_app.repositories.KardexRepository;
 import com.myproject.tool_rent_app.repositories.KardexTypeRepository;
 import com.myproject.tool_rent_app.repositories.ToolRepository;
+import com.myproject.tool_rent_app.repositories.ToolStateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +20,16 @@ public class ToolService {
     ToolRepository toolRepository;
 
     @Autowired
+    ToolStateRepository toolStateRepository;
+
+    @Autowired
     KardexRepository kardexRepository;
 
     @Autowired
     KardexTypeRepository kardexTypeRepository;
 
+    // RF 1.1: Registrar nuevas herramientas con datos básicos (nombre, categoría, estado
+    // inicial, valor de reposición)
     public ToolEntity createTool(ToolEntity tool){
         ToolEntity savedTool = toolRepository.save(tool);
 
@@ -30,8 +37,11 @@ public class ToolService {
         kardex.setTool(savedTool);
 
         // Check if kardex type name exists in the DB
-        KardexTypeEntity entryType = kardexTypeRepository.findByName("Ingreso")
-                .orElseThrow(() -> new RuntimeException("Tipo de kardex no encontrado"));
+        KardexTypeEntity entryType = kardexTypeRepository.findByName("Ingreso");
+
+        if (entryType == null) {
+            throw new RuntimeException("Tipo de kardex 'Ingreso' no encontrado");
+        }
 
         kardex.setType(entryType);
         kardex.setMovementDate(LocalDateTime.now());
@@ -40,5 +50,19 @@ public class ToolService {
         kardexRepository.save(kardex);
 
         return savedTool;
+    }
+    // RF 1.2: Dar de baja herramientas dañadas o en desuso.
+    public void deleteTool(ToolEntity tool){
+        ToolStateEntity unserviceableState = toolStateRepository.findByName("Dada de baja");
+
+        if (unserviceableState == null){
+            throw new RuntimeException("Estado 'Dado de baja' no encontrado");
+        }
+
+        if (tool.getCurrentState().equals(unserviceableState)) {
+            toolRepository.delete(tool);
+        } else {
+            throw new RuntimeException("La herramienta no está dada de baja, no se puede eliminar");
+        }
     }
 }
