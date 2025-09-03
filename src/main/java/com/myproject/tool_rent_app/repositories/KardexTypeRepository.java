1ac6b34160b4c1a4467ca6907238d6f0f6a4a787
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-09-02 20:49:33 -0400

    Updated KardexTypeRepository findByName method.

--- src/main/java/com/myproject/tool_rent_app/repositories/KardexTypeRepository.java
+++ src/main/java/com/myproject/tool_rent_app/repositories/KardexTypeRepository.java
@@ -3,8 +3,7 @@ package com.myproject.tool_rent_app.repositories;
 import com.myproject.tool_rent_app.entities.KardexTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface KardexTypeRepository extends JpaRepository<KardexTypeEntity,Long> {
-    Optional<KardexTypeEntity> findByName(String name);
+    KardexTypeEntity findByName(String name);
 }
