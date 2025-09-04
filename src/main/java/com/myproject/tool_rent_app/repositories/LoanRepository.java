b39e4d5d22a85b309421cbdb1a7c255e95c845ff
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-09-03 22:12:00 -0400

    Added new findByLoanId method in LoanRepository.

--- src/main/java/com/myproject/tool_rent_app/repositories/LoanRepository.java
+++ src/main/java/com/myproject/tool_rent_app/repositories/LoanRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends JpaRepository<LoanEntity,Long> {
+    LoanEntity findByLoanId(Long loanId);
 }
